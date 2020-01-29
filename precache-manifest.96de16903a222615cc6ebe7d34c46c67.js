d7b43a4a9372cdb032b8aa21390bf01704bf6706
daphne Fang <daphneys.fang@gmail.com>
2020-01-28 22:41:17 -0800

    Updates

---
+++ precache-manifest.96de16903a222615cc6ebe7d34c46c67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a279e7cbf27c4dbb45f5427ae2b67379",
+    "url": "/laptop-customizer/index.html"
+  },
+  {
+    "revision": "b14526bc7546dce47851",
+    "url": "/laptop-customizer/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/laptop-customizer/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "b14526bc7546dce47851",
+    "url": "/laptop-customizer/static/js/main.b53204c2.chunk.js"
+  },
+  {
+    "revision": "499f4b4adf8f43d881a8",
+    "url": "/laptop-customizer/static/js/runtime~main.9cc997fb.js"
+  }
+]);
